89e6a3d3a778383132f4475f2123fa175e629f4c
root <alexboy60318@gmail.com>
2020-08-20 05:21:45 -0400

    enumer4liunx completed!

--- scanner.py
+++ scanner.py
@@ -1,7 +1,6 @@
-import masscan,json,subprocess,asyncio
+import masscan,json,subprocess,asyncio,threading
 
 class SMB_scanner:
-
     def __init__(self,ipaddress):
 
         print( "  _____ __  __ ____   _____" +                               
@@ -16,7 +15,7 @@ class SMB_scanner:
     def port_scanning(self,ipaddress):
 
         """
-        This function is used to scan throug smb port 139,445
+        This function is used to scan through smb portocol by port 139,445
         """
 
         ips_445 = []
@@ -35,10 +34,33 @@ class SMB_scanner:
                 result = loop.run_until_complete(self.smb_445_enumeration(ip))
                 print(ip+" port 445 enumeration "+result)
                 ips_445.append(ip)
-        # self.nmap_smb_enumeration(ips_139,ips_445)
-    
+
+    def enum4liunx(self,ip):
+        try:
+            result = subprocess.check_output(['enum4linux','-a',ip],stderr=subprocess.STDOUT)
+            result = result.decode('utf-8')
+            print(ip + " in enum4linux!!!  "+ result)
+        except subprocess.CalledProcessError as e:
+            # raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+            result =e.output.decode('utf-8')
+            print(ip + " in enum4linux!!!  "+ result)
+    def nmap(self,ip):
+        # subprocess.run(['enum4linux','-a',ip])
+        print(ip+" in nmap!!!")
+
     async def smb_445_enumeration(self,ip):
-    
+        threads = []
+
+        self.enum4liunx(ip)
+        self.nmap(ip)
+        # task1 = threading.Thread(target = enum4liunx,args=(ip))
+        # task1.start()
+        # threads.append(task1)
+        # task2 = threading.Thread(target = nmap,args=(ip))
+        # task2.start()
+        # threads.append(task2)
+        # for thread in threads:
+        #     thread.join()
         return "success"
     async def smb_139_enumeration(self,ip):
  
