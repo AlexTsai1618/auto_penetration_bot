78436039cbdb32d54bb24c6dfb93d9de37c3b985
root <alexboy60318@gmail.com>
2020-08-24 04:03:10 -0400

    add nmap 445 function

--- scanner.py
+++ scanner.py
@@ -1,4 +1,4 @@
-import masscan,json,subprocess,asyncio,threading
+import masscan,json,subprocess,asyncio,threading,nmap
 
 class SMB_scanner:
     def __init__(self,ipaddress):
@@ -20,12 +20,24 @@ class SMB_scanner:
 
         ips_445 = []
         ips_139 = []
+        ips_137 = []
+        ips_138 = []
         mas = masscan.PortScanner()
-        mas.scan(ipaddress, ports='445,139', arguments='--max-rate 1000')
+        mas.scan(ipaddress, ports='445,139,137,138', arguments='--max-rate 1000')
         data = mas.scan_result
         loop = asyncio.new_event_loop()
         
         for ip in data['scan']:
+            print(data['scan'][ip])
+            # if data['scan'][ip]['udp'][137]['state']=="open":
+            #     result = loop.run_until_complete(self.smb_137_enumeration(ip))
+            #     print(ip+" port 137 enumeration "+result)
+            #     ips_137.append(ip)
+            # if data['scan'][ip]['udp'][138]['state']=="open":
+            #     result = loop.run_until_complete(self.smb_138_enumeration(ip))
+            #     print(ip+" port 138 enumeration "+result)
+            #     ips_138.append(ip)
+            
             if data['scan'][ip]['tcp'][139]['state']=="open":
                 result = loop.run_until_complete(self.smb_139_enumeration(ip))
                 print(ip+" port 139 enumeration "+result)
@@ -36,32 +48,59 @@ class SMB_scanner:
                 ips_445.append(ip)
 
     def enum4liunx(self,ip):
+        print(ip)
         try:
             result = subprocess.check_output(['enum4linux','-a',ip],stderr=subprocess.STDOUT)
             result = result.decode('utf-8')
             print(ip + " in enum4linux!!!  "+ result)
         except subprocess.CalledProcessError as e:
             # raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
-            result =e.output.decode('utf-8')
+            result = e.output.decode('utf-8')
             print(ip + " in enum4linux!!!  "+ result)
-    def nmap(self,ip):
-        # subprocess.run(['enum4linux','-a',ip])
-        print(ip+" in nmap!!!")
-
+    def nmap_445_os_discovery(self,ip):
+        nm = nmap.PortScanner()
+        data = nm.scan(ip, '445', arguments='--script=./nmap_script/scripts/smb-os-discovery.nse')
+        with open('./report/'+ip+"txt",'W') as file:
+            file.write(data)
+        print(data)
+        print(ip+" in nmap_445_os_discovery!!!")
+    def nmap_445_enum_shares(self,ip):
+        nm = nmap.PortScanner()
+        data = nm.scan(ip, '445', arguments='--script=./nmap_script/scripts/smb-enum-shares.nse')
+        print(data)
+        print(ip+" in nmap_445_os_discovery!!!")
+    # def nmap_445(self,ip):
+    #     async nmap_
+    #     print(ip+" in nmap!!!")
+    # def nmap_445(self,ip):
+    #     async nmap_
+    #     print(ip+" in nmap!!!")
+    
     async def smb_445_enumeration(self,ip):
         threads = []
 
-        self.enum4liunx(ip)
-        self.nmap(ip)
-        # task1 = threading.Thread(target = enum4liunx,args=(ip))
-        # task1.start()
-        # threads.append(task1)
-        # task2 = threading.Thread(target = nmap,args=(ip))
-        # task2.start()
+        # self.enum4liunx(ip)
+        # self.nmap(ip)
+        print(ip)
+        task1 = threading.Thread(target = self.enum4liunx,args=(ip,))
+        task1.start()
+        task1.join()
+        task2 = threading.Thread(target = self.nmap_445_os_discovery,args=(ip,))
+        task2.start()
+        task2.join()
         # threads.append(task2)
         # for thread in threads:
         #     thread.join()
         return "success"
+
+    async def smb_137_enumeration(self,ip):
+ 
+        return "success"
+
+    async def smb_138_enumeration(self,ip):
+ 
+        return "success"
+
     async def smb_139_enumeration(self,ip):
  
         return "success"
