15fa3fdcfb532766f17c26f765e5f3cb7425b6c1
alexboy60318 <alexboy60318@gmail.com>
2020-09-11 04:58:46 -0400

    parsing

--- data_clean.py
+++ data_clean.py
@@ -5,7 +5,17 @@ def report_generate():
 def data_collection():
 
 def share_clean(raw_file):
-
+    data = {}
+    tables =  parsed_file['nmaprun']['host']['hostscript']["script"]['table']
+    temp = []
+    for i in tables:
+        temp.append(i['elem'][2]["@key"])
+        temp.append(i['elem'][2]["#text"])
+        temp.append(i['elem'][3]["@key"])
+        temp.append(i['elem'][3]["#text"])
+        data[i["@key"]] = temp
+        temp = []
+    return data
 def os_clean(raw_file):
     with open(raw_file,'w')as file:
         os = parsed_file['nmaprun']['host']['hostscript']["script"]['elem'][0]['#text']
