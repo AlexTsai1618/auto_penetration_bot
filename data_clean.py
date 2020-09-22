d27cfb898031c3cbd331db4dc54457fd3de1e6fa
alexboy60318 <alexboy60318@gmail.com>
2020-09-22 04:36:38 -0400

    try except add added

--- data_clean.py
+++ data_clean.py
@@ -22,6 +22,7 @@ def schedual(files):
     ms08067_data = nmap_datas([e for e in files if e.endswith(filename.MS08)][0])
     ms10054_data = nmap_datas([e for e in files if e.endswith(filename.MS10)][0])
     ms10061_data = nmap_datas([e for e in files if e.endswith(filename.MS10_2)][0])
+    ms17010_data = handle_txt_file([e for e in files if e.endswith(filename.MS17)][0])
     cve_2020_0796_data = handle_txt_file([e for e in files if e.endswith(filename.CVE2020)][0])
     cve_2020_1206_data = handle_txt_file([e for e in files if e.endswith(filename.CVE2020_2)][0])
     datas = {
@@ -37,7 +38,7 @@ def schedual(files):
         "ms08-067":ms08067_data,
         "ms10-054":ms10054_data,
         "ms10-061":ms10061_data,
-        # "ms17-010":ms17010_data,
+        "ms17-010":ms17010_data,
 
     }
     print(datas)
@@ -63,50 +64,52 @@ def data_path():
     thread_list = []
     for directory in directories:
         temp_path = os.path.join('data/raw',directory)
-        # print(temp_path)
         temp_files = os.listdir(temp_path)
-        # print(temp_files)
         files = [os.path.join(temp_path,file) for file in temp_files]
-        # print(files)
-        
         task = threading.Thread(target=schedual, args=(files,))
         task.start()
         thread_list.append(task)
     for thread in thread_list:
         thread.join()
 def share_clean(raw_file):
-    raw_file = open(raw_file,"rb")
-    parsed_file = xmltodict.parse((raw_file))
-    data = {}
-    tables =  parsed_file['nmaprun']['host']['hostscript']["script"]['table']
-    temp = []
-    for i in tables:
-        temp.append(i['elem'][2]["@key"])
-        temp.append(i['elem'][2]["#text"])
-        temp.append(i['elem'][3]["@key"])
-        temp.append(i['elem'][3]["#text"])
-        data[i["@key"]] = temp
-        temp = []
-    return data
+    with open(raw_file,"rb") as raw_file:
+        parsed_file = xmltodict.parse(raw_file)
+        try:
+            data = {}
+            tables =  parsed_file['nmaprun']['host']['hostscript']["script"]['table']
+            temp = []
+            for i in tables:
+                temp.append(i['elem'][2]["@key"])
+                temp.append(i['elem'][2]["#text"])
+                temp.append(i['elem'][3]["@key"])
+                temp.append(i['elem'][3]["#text"])
+                data[i["@key"]] = temp
+                temp = []
+            return data
+        except:
+            data = "NULL"
+            return data
 def os_clean(raw_file):
     data = {}
     with open(raw_file,'rb')as file:
         parsed_file = xmltodict.parse((file))
-        
-        os = parsed_file['nmaprun']['host']['hostscript']["script"]['elem'][0]['#text']
-        fqdn = parsed_file['nmaprun']['host']['hostscript']["script"]['elem'][4]['#text']
-        workgroup = parsed_file['nmaprun']['host']['hostscript']["script"]['elem'][6]['#text']
-    return os,fqdn,workgroup
+        parsed_file2 = parsed_file['nmaprun']['host']['hostscript']["script"]['elem']
+        try:
+            os = parsed_file2[0]['#text']
+            fqdn = parsed_file2[4]['#text']
+            workgroup = parsed_file2[6]['#text']
+            return os,fqdn,workgroup
+        except:
+            return "NULL","NULL","NULL","NULL"
 def account_clean(raw_file):
     with open(raw_file,'r') as file:
-        raw = file.read()
-        # print(raw)        
-        clean_data = raw[50::].split(' ')
-        account = clean_data[0]
-        password = clean_data[1]
-        
-    return account,password
-
+        try:
+            raw = [file for file in file.readlines() if file.startswith("ACCOUNT FOUND:")][0]
+            clean_data = raw[50::].split(' ')
+            account = clean_data[0]
+            password = clean_data[2]
+            return account,password
+        except:
+            return "NULL","NULL"
 if __name__ == "__main__":
     data_path()
-    subprocess.run(['sudo','rm','-rf','data/raw/*'])
