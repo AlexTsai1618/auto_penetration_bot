8adf207707faa8a67ab8d05911ba557534a2c8ba
root <alexboy60318@gmail.com>
2020-09-25 00:23:11 -0400

    fix sth

--- report_generator.py
+++ report_generator.py
@@ -6,11 +6,22 @@ def report(datas):
 def data_count(datas):
     ips = []
     computer_os = []
+    os_count = {
+        "Windows 10":0,
+        "Windows 7":0,
+        "Windows 8":0,
+        "Windows XP":0,
+        "Windows Server 2016":0,
+        "Windows Server 2012 R2":0,
+        "Windows Server 2008 R2":0,
+        "Windows Server 2008":0
+    }
     for data in datas:
         file = open(data)
         data = json.load(file)
         ips.append(data['ip'])
         computer_os.append(data['os'])
+    
     print(len(ips),computer_os)
 
 def paths():
